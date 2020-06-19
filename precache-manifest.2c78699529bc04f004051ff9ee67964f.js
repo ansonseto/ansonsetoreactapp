3712b856a8fcfd2b7c2f5e53a62938c60eb65bfa
ansonseto <61979765+ansonseto@users.noreply.github.com>
2020-06-19 13:16:30 +0100

    Updates

---
+++ precache-manifest.2c78699529bc04f004051ff9ee67964f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf321b2fedc3d15cc4792ef6732b603e",
+    "url": "/ansonsetoreactapp/index.html"
+  },
+  {
+    "revision": "d647b016b9abd9a7ed0f",
+    "url": "/ansonsetoreactapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6acc6d50a769415cc4e7",
+    "url": "/ansonsetoreactapp/static/js/2.b8533588.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ansonsetoreactapp/static/js/2.b8533588.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d647b016b9abd9a7ed0f",
+    "url": "/ansonsetoreactapp/static/js/main.1c9f4f25.chunk.js"
+  },
+  {
+    "revision": "2fe369f2a9bb00562bc6",
+    "url": "/ansonsetoreactapp/static/js/runtime-main.55c7bbf5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ansonsetoreactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
